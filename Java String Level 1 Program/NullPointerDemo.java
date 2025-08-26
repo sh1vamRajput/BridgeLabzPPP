9f2291428ab0c96c59837bfdbdc46100c0c5e223
Shivam Rajput <149651072+sh1vamRajput@users.noreply.github.com>
2025-08-26 18:04:29 +0530

    Create NullPointerDemo.java

---
+++ Java String Level 1 Program/NullPointerDemo.java
@@ -0,0 +1,23 @@
+class NullPointerDemo {
+
+    
+    static void generateException() {
+        String text = null;
+        System.out.println(text.length()); // NPE
+    }
+
+    
+    static void handleException() {
+        try {
+            String text = null;
+            System.out.println(text.length());
+        } catch (NullPointerException e) {
+            System.out.println("Caught NullPointerException!");
+        }
+    }
+
+    public static void main(String[] args) {
+       
+        handleException();
+    }
+}
