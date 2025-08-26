8880711b0f2ec863e64ce16d88442779073b46ce
Shivam Rajput <149651072+sh1vamRajput@users.noreply.github.com>
2025-08-26 18:00:40 +0530

    Create ToCharArrayCompare.java

---
+++ Java String Level 1 Program/ToCharArrayCompare.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+class ToCharArrayCompare {
+
+
+    static char[] getChars(String str) {
+        char[] arr = new char[str.length()];
+        for (int i = 0; i < str.length(); i++) {
+            arr[i] = str.charAt(i);
+        }
+        return arr;
+    }
+
+    
+    static boolean compareArrays(char[] arr1, char[] arr2) {
+        if (arr1.length != arr2.length) return false;
+        for (int i = 0; i < arr1.length; i++) {
+            if (arr1[i] != arr2[i]) return false;
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a string: ");
+        String text = sc.next();
+
+        char[] userDefined = getChars(text);
+        char[] builtIn = text.toCharArray();
+
+        System.out.println("Comparison Result: " + compareArrays(userDefined, builtIn));
+        sc.close();
+    }
+}
