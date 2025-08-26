9593f8ecf21cc05d9aaa282372650c86cc697706
Shivam Rajput <149651072+sh1vamRajput@users.noreply.github.com>
2025-08-26 18:06:59 +0530

    Create NumberFormatDemo.java

---
+++ Java String Level 1 Program/NumberFormatDemo.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+class NumberFormatDemo {
+
+    static void generateException(String text) {
+        int num = Integer.parseInt(text); 
+        System.out.println(num);
+    }
+
+    static void handleException(String text) {
+        try {
+            int num = Integer.parseInt(text);
+            System.out.println(num);
+        } catch (NumberFormatException e) {
+            System.out.println("Caught NumberFormatException!");
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter text: ");
+        String text = sc.next();
+
+        
+        handleException(text);
+        sc.close();
+    }
+}
