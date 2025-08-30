be3684b17aff031b3820dd05d447dbbe5822d45d
Shivam Rajput <149651072+sh1vamRajput@users.noreply.github.com>
2025-08-30 14:48:36 +0530

    Delete HelloWorld.java

--- HelloWorld.java
+++
@@ -1,9 +0,0 @@
-package BridgeLabzPPP;
-
-class HelloWorld {
-    public static void main(String[] args) {
-        System.out.println("Hello, World!");
-        System.out.println("Welcome to the BridgeLabz Programming Practice Program!");
-        System.out.println("This is a simple Java program to demonstrate basic syntax.");
-    }
-}
