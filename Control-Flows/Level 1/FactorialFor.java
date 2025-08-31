4c587454962b6475c8575e16a7efe1ed9e7efe4e
Shivam Rajput <149651072+sh1vamRajput@users.noreply.github.com>
2025-08-31 14:45:38 +0530

    Create FactorialFor.java

---
+++ Control-Flows/Level 1/FactorialFor.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+class FactorialFor {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a positive integer: ");
+        int n = sc.nextInt();
+
+        if (n < 0) {
+            System.out.println("Factorial is not defined for negative numbers.");
+            return;
+        }
+
+        int fact = 1;
+        for (int i = 1; i <= n; i++) {
+            fact *= i;
+        }
+
+        System.out.println("Factorial of " + n + " is: " + fact);
+    }
+}
