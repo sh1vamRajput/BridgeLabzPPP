2b511c748d326cdee66a3d56c9133587c3a63bd9
Shivam Rajput <149651072+sh1vamRajput@users.noreply.github.com>
2025-08-31 12:51:01 +0530

    Create BMICalculator2D.java

---
+++ Arrays/Level 2/BMICalculator2D.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+class BMICalculator2D {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter number of persons: ");
+        int n = sc.nextInt();
+
+        double[][] personData = new double[n][3]; 
+        String[] status = new String[n];
+
+        for (int i = 0; i < n; i++) {
+            System.out.print("Enter weight (kg) of person " + (i + 1) + ": ");
+            personData[i][0] = sc.nextDouble();
+            System.out.print("Enter height (m) of person " + (i + 1) + ": ");
+            personData[i][1] = sc.nextDouble();
+
+            personData[i][2] = personData[i][0] / (personData[i][1] * personData[i][1]);
+
+            if (personData[i][2] < 18.5) status[i] = "Underweight";
+            else if (personData[i][2] < 25) status[i] = "Normal";
+            else if (personData[i][2] < 30) status[i] = "Overweight";
+            else status[i] = "Obese";
+        }
+
+        System.out.println("\n--- BMI Report ---");
+        for (int i = 0; i < n; i++) {
+            System.out.println("Person " + (i + 1) + " | Height: " + personData[i][1] + "m | Weight: " + personData[i][0] + "kg | BMI: " + personData[i][2] + " | Status: " + status[i]);
+        }
+    }
+}
