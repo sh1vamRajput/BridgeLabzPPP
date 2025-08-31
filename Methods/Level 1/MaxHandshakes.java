de65a49af3ed98d05e110df67598a64ed5cda5cf
Shivam Rajput <149651072+sh1vamRajput@users.noreply.github.com>
2025-08-31 15:52:23 +0530

    Create MaxHandshakes.java

---
+++ Methods/Level 1/MaxHandshakes.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+class MaxHandshakes {
+    
+    public static int calculateHandshakes(int n) {
+        return (n * (n - 1)) / 2;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter number of students: ");
+        int n = sc.nextInt();
+
+        int handshakes = calculateHandshakes(n);
+        System.out.println("Maximum possible handshakes = " + handshakes);
+
+        sc.close();
+    }
+}
