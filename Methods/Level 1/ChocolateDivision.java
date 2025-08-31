30f365b401151bdf7bdb92a0c8138e63851634ad
Shivam Rajput <149651072+sh1vamRajput@users.noreply.github.com>
2025-08-31 15:57:45 +0530

    Create ChocolateDivision.java

---
+++ Methods/Level 1/ChocolateDivision.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+class ChocolateDivision {
+    public static int[] findRemainderAndQuotient(int chocolates, int children) {
+        int perChild = chocolates / children;
+        int remainder = chocolates % children;
+        return new int[] {perChild, remainder};
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter number of chocolates: ");
+        int chocolates = sc.nextInt();
+
+        System.out.print("Enter number of children: ");
+        int children = sc.nextInt();
+
+        int[] result = findRemainderAndQuotient(chocolates, children);
+        System.out.println("Each child gets " + result[0] + " chocolates, Remaining = " + result[1]);
+
+        sc.close();
+    }
+}
