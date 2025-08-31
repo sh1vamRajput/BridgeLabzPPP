1e4fe93fe7b86a707cc5773295a0fc1e510b8860
Shivam Rajput <149651072+sh1vamRajput@users.noreply.github.com>
2025-08-31 16:41:21 +0530

    Create Quadratic.java

---
+++ Methods/Level 2/Quadratic.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Quadratic {
+
+    static double[] findRoots(double a, double b, double c) {
+        double delta = b * b - 4 * a * c;
+        if (delta < 0) return new double[] {};
+        else if (delta == 0) return new double[] { -b / (2 * a) };
+        else {
+            double root1 = (-b + Math.sqrt(delta)) / (2 * a);
+            double root2 = (-b - Math.sqrt(delta)) / (2 * a);
+            return new double[] { root1, root2 };
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter coefficients a, b, c: ");
+        double a = sc.nextDouble(), b = sc.nextDouble(), c = sc.nextDouble();
+
+        double[] roots = findRoots(a, b, c);
+        if (roots.length == 0) System.out.println("No Real Roots");
+        else {
+            System.out.println("Roots: ");
+            for (double r : roots) System.out.println(r);
+        }
+    }
+}
