5adfd8aea101b6218d0b4007c040589015ce1b22
Shivam Rajput <149651072+sh1vamRajput@users.noreply.github.com>
2025-08-31 20:47:11 +0530

    Create Handshakes.java

---
+++ Programming-Elements/Level 1/Handshakes.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+class Handshakes {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter number of students: ");
+        int n = sc.nextInt();
+        int handshakes = (n * (n - 1)) / 2;
+        System.out.println("The maximum number of handshakes is " + handshakes);
+    }
+}
