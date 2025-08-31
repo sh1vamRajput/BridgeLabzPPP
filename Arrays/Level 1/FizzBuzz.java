05ce06fd8941462968178c5947b8293d46bb6a0a
Shivam Rajput <149651072+sh1vamRajput@users.noreply.github.com>
2025-08-31 12:43:21 +0530

    Create FizzBuzz.java

---
+++ Arrays/Level 1/FizzBuzz.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+class FizzBuzz {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int num = sc.nextInt();
+
+        if (num <= 0) {
+            System.out.println("Enter a positive number.");
+            return;
+        }
+
+        String[] result = new String[num + 1];
+
+        for (int i = 1; i <= num; i++) {
+            if (i % 3 == 0 && i % 5 == 0) result[i] = "FizzBuzz";
+            else if (i % 3 == 0) result[i] = "Fizz";
+            else if (i % 5 == 0) result[i] = "Buzz";
+            else result[i] = String.valueOf(i);
+        }
+
+        for (int i = 1; i <= num; i++) {
+            System.out.println("Position " + i + " = " + result[i]);
+        }
+    }
+}
